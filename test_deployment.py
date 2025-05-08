77fb35fc5b0606698485c29e49bf4d98848d91d6
Niamh K <kennedy.niamh@live.ie>
2025-05-08 13:34:24 +0100

    Initial commit

---
+++ test_deployment.py
@@ -0,0 +1,157 @@
+import smtplib
+import dns.resolver
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+
+# Global Variables
+# Test Domain
+domain = "projectauthentication.com"
+
+# Test Email Sender
+sender = "from-addr@projectauthentication.com"
+
+# Test Email Recipients
+recipients = ["thelncproject3@gmail.com", "thelncproject3@outlook.ie", "thelncproject3@yahoo.com"]
+
+# Authenticated Outlook SMTP
+outlook_smtp_server = "smtp-mail.outlook.com"
+outlook_smtp_port = 587
+outlook_smtp_user = sender
+outlook_smtp_password = "kpdwppzfjyhswhxn"
+
+# Postfix SMTP
+postfix_smtp_server = "localhost"
+postfix_smtp_port = 25
+
+# Check DNS records for the provided domain to verify TXT record exists for SPF
+def check_spf(domain):
+    try:
+        answers = dns.resolver.resolve(domain, 'TXT')
+        for rdata in answers:
+            if 'v=spf1' in rdata.to_text():
+                return "Y"
+        return "N"
+    except:
+        return "N"
+
+# Check DNS records for the provided domain to verify CNAME record exists for DKIM
+def check_dkim(domain, selector="1"):
+    try:
+        dkim_domain = f"selector{selector}._domainkey.{domain}"
+        answers = dns.resolver.resolve(dkim_domain, 'CNAME')
+        if answers:
+            return "Y"
+        return "N"
+    except:
+        return "N"
+
+# Check DNS records for the provided domain to verify TXT record exists for DMARC
+def check_dmarc(domain):
+    try:
+        dmarc_domain = f"_dmarc.{domain}"
+        answers = dns.resolver.resolve(dmarc_domain, 'TXT')
+        if answers:
+            return "Y"
+        return "N"
+    except:
+        return "N"
+
+# Load Email Templates
+def load_email_template(template_file):
+    with open(template_file, "r") as f:
+        return f.read()
+
+# Send Emails by Test Case and Email Template
+def send_email(smtp_server, smtp_port, sender, recipient, subject, body, smtp_user=None, smtp_password=None):
+    msg = MIMEMultipart()
+    msg['From'] = sender
+    msg['To'] = recipient
+    msg['Subject'] = subject
+    msg.attach(MIMEText(body, 'html'))
+
+    try:
+        with smtplib.SMTP(smtp_server, smtp_port) as server:
+            # Secure Connection
+            server.starttls()
+
+            # Authenticate with SMTP server if credentials provided with test case
+            if smtp_user:
+                server.login(smtp_user, smtp_password)
+
+            # Send email to recipient with subject and body template for this test case
+            server.sendmail(sender, recipient, msg.as_string())
+
+            # Print message if email was sent successfully
+            print(f"Email successfully sent!")
+
+    except Exception as e:
+        #Print message if email failed to send
+        print(f"Failed to send email: {e}")
+
+# Test Case 1: Outlook SMTP, Legitimate email templates
+def send_test_case_1(test_phase):
+    for recipient in recipients:
+        for i in range(1,4): # Sending three email templates
+            subject = "Test Phase #" + str(test_phase) + " - Test Case #1 - Legit Email #" + str(i)
+            print("Sending: " + subject + " - TO: " + recipient)
+            template = load_email_template(f"emails/legit_email_{i}.html")
+            send_email(outlook_smtp_server, outlook_smtp_port, sender, recipient, subject, template, outlook_smtp_user, outlook_smtp_password)
+
+# Test Case 2: Outlook SMTP, Phishing email templates
+def send_test_case_2(test_phase):
+    for recipient in recipients:
+        for i in range(1,4): # Sending three email templates
+            subject = "Test Phase #" + str(test_phase) + " - Test Case #2 - Phish Email #"
+            print("Sending: " + subject + " - TO: " + recipient)
+            template = load_email_template(f"emails/phish_email_{i}.html")
+            send_email(outlook_smtp_server, outlook_smtp_port, sender, recipient, subject, template, outlook_smtp_user, outlook_smtp_password)
+
+# Test Case 3: Postfix SMTP, Legitimate email templates
+def send_test_case_3(test_phase):
+    for recipient in recipients:
+        for i in range(1,4): # Sending three email templates
+            subject = "Test Phase #" + str(test_phase) + " - Test Case #3 - Legit Email #" + str(i)
+            print("Sending: " + subject + " - TO: " + recipient)
+            template = load_email_template(f"emails/legit_email_{i}.html")
+            send_email(postfix_smtp_server, postfix_smtp_port,sender, recipient, subject, template)
+
+# Test Case 4: Postfix SMTP, Phishing email templates
+def send_test_case_4(test_phase):
+    for recipient in recipients:
+        for i in range(1,4): # Sending three email templates
+            subject = "Test Phase #" + str(test_phase) + " - Test Case #4 - Phish Email #" + str(i)
+            print("Sending: " + subject + " - TO: " + recipient)
+            template = load_email_template(f"emails/phish_email_{i}.html")
+            send_email(postfix_smtp_server, postfix_smtp_port, sender, recipient, subject, template)
+
+def main():
+    # Configure which test phase is to be deployed - change as testing progresses
+    test_phase = 4
+    print(f"\n--- Test Phase {test_phase} ---\n")
+
+    # Confirming SPF, DKIM and DMARC configured for the domain
+    print(f"--- Verifying current SPF, DKIM and DMARC configuration in DNS records for {domain}---")
+    spf_result = check_spf(domain)
+    dkim_result = check_dkim(domain)
+    dmarc_result = check_dmarc(domain)
+
+    print("SPF:     " + spf_result)
+    print("DKIM:    " + dkim_result)
+    print("DMARC:   " + dmarc_result)
+
+    # Deploy emails for each test case
+    '''print(f"\n--- Deploying Test Emails Test Phase {test_phase} ---\n")
+    print(f"\n--- Sending Emails for Test Case 1 ---")
+    send_test_case_1(test_phase)
+    print(f"\n--- Sending Emails for Test Case 2 ---")
+    send_test_case_2(test_phase)
+    print(f"\n--- Sending Emails for Test Case 3 ---")
+    send_test_case_3(test_phase)
+    print(f"\n--- Sending Emails for Test Case 4 ---")
+    send_test_case_4(test_phase)
+
+    print(f"\n--- All Test Emails Sent for Test Phase {test_phase} ---\n")'''
+
+if __name__ == "__main__":
+    main()
---
+++ test_result_logging.py
@@ -0,0 +1,170 @@
+import csv
+import dns.resolver
+import imaplib
+
+
+# Global Variables
+# Test Domain
+domain = "projectauthentication.com"
+
+# Test Email Sender
+sender = "send-from@projectauthentication.com"
+
+# Test Email Recipients
+recipients = ["thelncproject3@gmail.com", "thelncproject3@outlook.ie", "thelncproject3@yahoo.com"]
+
+
+def check_spf(domain):
+    try:
+        answers = dns.resolver.resolve(domain, 'TXT')
+        for rdata in answers:
+            if 'v=spf1' in rdata.to_text():
+                return "Y"
+        return "N"
+    except:
+        return "N"
+
+
+def check_dkim(domain, selector="1"):
+    try:
+        dkim_domain = f"selector{selector}._domainkey.{domain}"
+        answers = dns.resolver.resolve(dkim_domain, 'CNAME')
+        if answers:
+            return "Y"
+        return "N"
+    except:
+        return "N"
+
+
+def check_dmarc(domain):
+    try:
+        dmarc_domain = f"_dmarc.{domain}"
+        answers = dns.resolver.resolve(dmarc_domain, 'TXT')
+        if answers:
+            return "Y"
+        return "N"
+    except:
+        return "N"
+
+# Check Gmail recipient inbox
+def check_gmail_recipient_status(recipient, subject):
+    try:
+        mail = imaplib.IMAP4_SSL("imap.gmail.com")
+        mail.login(recipient,"stlfozipuuzhfugf ")
+
+        # Check Spam folder first
+        status, _ = mail.select("[Gmail]/Spam")
+        if status == "OK":
+            result, data = mail.search(None, f'(FROM "{sender}" SUBJECT "{subject}")')
+            if data[0]:
+                return "Spam"
+
+        # Check Inbox last
+        mail.select("Inbox")
+        result, data = mail.search(None, f'(FROM "{sender}" SUBJECT "{subject}")')
+        if data[0]:
+            return "Inbox"
+
+        return "Not Found"
+    except Exception as e:
+        return f"Error checking status: {e}"
+    finally:
+        if mail:
+            try:
+                mail.logout()
+            except:
+                pass
+
+# Check Outlook recipient inbox
+def check_outlook_recipient_status(recipient, subject):
+    mail = None
+    try:
+        mail = imaplib.IMAP4_SSL("outlook.office365.com",993)
+        mail.login(recipient, "wwljxtuejtczrmgq")
+
+        # Check Junk folder first
+        status, _ = mail.select("Junk Email")
+        if status == "OK":
+            result, data = mail.search(None, f'(FROM "{sender}" SUBJECT "{subject}")')
+            if data and data[0].strip():
+                return "Spam"
+
+        # Check Inbox only if not found in Junk
+        status, _ = mail.select("Inbox")
+        if status == "OK":
+            result, data = mail.search(None, f'(FROM "{sender}" SUBJECT "{subject}")')
+            if data and data[0].strip():
+                return "Inbox"
+
+        return "Not Found"
+    except Exception as e:
+        return f"Error checking status: {e}"
+    finally:
+        if mail:
+            try:
+                mail.logout()
+            except:
+                pass
+
+# Log test phase results into cvs file
+def log_results(results, filename="email_test_log.csv"):
+    with open(filename, mode='a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(results)
+
+def test_result_module(test_phase):
+    print(f"\n--- Checking Test Recipient Inboxes and Assembling Test Results for Test Phase {test_phase} ---\n")
+    # Create list to store test result for each email sent
+    email_results = []
+
+    spf_result = check_spf(domain)
+    dkim_result = check_dkim(domain)
+    dmarc_result = check_dmarc(domain)
+
+    # Check email status by recipient inbox
+    for recipient in recipients:
+        for a in range(1, 5):
+            for b in range(1, 4):
+                # Iterating through test email subjects
+                if a % 2 == 0:
+                    subject = "Test Phase #" + str(test_phase) + " - Test Case #" + str(a) + " - Phish Email #" + str(b)
+                    test_id = "P" + str(test_phase) + "-TC" + str(a) + "-PE" + str(b)
+                else:
+                    subject = "Test Phase #" + str(test_phase) + " - Test Case #" + str(a) + " - Legit Email #" + str(b)
+                    test_id = "P" + str(test_phase) + "-TC" + str(a) + "-LE" + str(b)
+
+                # Choose the appropriate checker based on recipient email provider
+                if "gmail.com" in recipient:
+                    email_status = check_gmail_recipient_status(recipient, subject)
+                elif "outlook.ie" in recipient:
+                    email_status = "Manual Check Required"
+                elif "yahoo.com" in recipient:
+                    email_status = "Manual Check Required"
+
+                # Adding email deposition result to the results list
+                email_results.append([str(test_phase), str(a), recipient, test_id, email_status])
+                #email_results.append([str(test_phase), spf_result, dkim_result, dmarc_result, str(a), recipient, test_id, email_status])
+
+    # Printing list of test results to the console
+    print("\n--- Displaying Test Results ---")
+    for result in email_results:
+        print(result)
+        # Attaching a timestamp each result and logging all to CSV file
+        log_results(result)
+
+    print("Results logged to CSV file.\n")
+
+    # Testing complete
+    print("\n--- Phase " + str(test_phase) + " Result Logging Complete ---")
+
+def main():
+    test_result_module(1)
+    # Compile test results for each phase of testing and output to CVS file
+    '''for test_phase in range(1,5):
+        test_result_module(test_phase)'''
+
+    # Testing complete
+    print("\n--- All Result Logging Complete ---")
+
+if __name__ == "__main__":
+    main()
